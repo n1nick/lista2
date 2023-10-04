7bfb5fd81406d0abd17c2337a26ee078915d272b
nini <113205557+n1nick@users.noreply.github.com>
2023-10-04 20:30:04 -0300

    Create questão1.c

---
+++ questão1.c
@@ -0,0 +1,36 @@
+int main() {
+
+double valordacompra, idade, numerosorteio;
+
+printf("Preencha para concorrer a um prêmio no final do mês.\n\n"); printf("Insira a sua idade: \n");
+
+scanf("%lf", &idade);
+
+printf("Insira o valor das suas compras: \n");
+
+scanf("%lf", &valordacompra);
+
+if(valordacompra < 500.0){
+
+numerosorteio = 5000 - (valordacompra idade);
+
+printf("Seu número da sorte é %.01f\n", numerosorteio);
+
+} else {
+
+if(valordacompra > 500.0) {
+
+numerosorteio = 50000 - (valordacompra idade); 
+printf("Seu número da sorte é %.0lf\n", numerosorteio); }
+
+if (valordacompra == 500.0){
+
+numerosorteio = idade * idade;
+
+printf("Seu número da sorte é %.0f\n", numerosorteio);
+
+}
+
+}
+
+return 0;
