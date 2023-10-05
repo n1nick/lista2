36ad2be196a3d85dff6bf971047613584d5c5628
nini <113205557+n1nick@users.noreply.github.com>
2023-10-04 21:20:17 -0300

    Update questão1.c

--- questão1.c
+++ questão1.c
@@ -1,36 +1,31 @@
+#include <stdio.h>
+
 int main() {
+    double valordacompra, idade, numerosorteio;
 
-double valordacompra, idade, numerosorteio;
+    printf("Preencha para concorrer a um prêmio no final do mês.\n\n");
+    printf("Insira a sua idade: \n");
 
-printf("Preencha para concorrer a um prêmio no final do mês.\n\n"); printf("Insira a sua idade: \n");
+    scanf("%lf", &idade);
 
-scanf("%lf", &idade);
+    printf("Insira o valor das suas compras: \n");
 
-printf("Insira o valor das suas compras: \n");
+    scanf("%lf", &valordacompra);
 
-scanf("%lf", &valordacompra);
+    if (valordacompra < 500.0) {
+        numerosorteio = 5000 - (valordacompra * idade);
+        printf("Seu número da sorte é %.1lf\n", numerosorteio);
+    } else {
+        if (valordacompra > 500.0) {
+            numerosorteio = 50000 - (valordacompra * idade);
+            printf("Seu número da sorte é %.0lf\n", numerosorteio);
+        }
 
-if(valordacompra < 500.0){
+        if (valordacompra == 500.0) {
+            numerosorteio = idade * idade;
+            printf("Seu número da sorte é %.0lf\n", numerosorteio);
+        }
+    }
 
-numerosorteio = 5000 - (valordacompra idade);
-
-printf("Seu número da sorte é %.01f\n", numerosorteio);
-
-} else {
-
-if(valordacompra > 500.0) {
-
-numerosorteio = 50000 - (valordacompra idade); 
-printf("Seu número da sorte é %.0lf\n", numerosorteio); }
-
-if (valordacompra == 500.0){
-
-numerosorteio = idade * idade;
-
-printf("Seu número da sorte é %.0f\n", numerosorteio);
-
+    return 0;
 }
-
-}
-
-return 0;
